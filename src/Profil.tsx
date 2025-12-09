43d1706c12edc3261a8e40adea50ec1f81723afd
betsaleeldjuma <djumabestaleel@gmail.com>
2025-12-09 19:41:29 +0200

    There's modifications

--- src/Profil.tsx
+++ src/Profil.tsx
@@ -2,16 +2,17 @@ import { useState } from "react"
 import { admin } from "./data/profil"
 
 const Profil = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpenDescription, setIsOpenDescription] = useState(false)
+    const [isProfil, setIsProfil] = useState(false)
 
   return (
     <div className="flex justify-between p-5 items-center text-[#C8ACD6]">
         {/* Salutation */}
         <div className="w-[40%]">
-            {!isOpen ? 
+            {!isOpenDescription ? 
             <div className="flex flex-col items-center gap-4">
                 <h1 className="font-bold text-5xl">Hi I'm Yujiro_Zenin</h1>
-                <button onClick={() => setIsOpen(!isOpen)} className="hover:text-white underline">View description</button>
+                <button onClick={() => setIsOpenDescription(!isOpenDescription)} className="hover:text-white underline">View description</button>
             </div>
             : <div className="flex flex-col gap-2">
                 <h1 className="font-bold text-4xl">Hi I'm Yujiro_Zenin</h1>
@@ -19,17 +20,19 @@ const Profil = () => {
                     <div>
                         <p className="text-2xl">{admin.description}</p>
                     </div>
-                    <button onClick={() => setIsOpen(!isOpen)} className="hover:text-white underline">Close description</button>
+                    <button onClick={() => setIsOpenDescription(!isOpenDescription)} className="hover:text-white underline">Close description</button>
                 </div>
             </div>}
         </div>
         {/* Profil */}
-        <div className="flex flex-col justify-center items-center w-[40%]">
-            <img src={admin.src} alt={admin.name} className="rounded-full"/>
-            <h1 className="text-2xl font-extrabold">{admin.name}</h1>
-            <h2 className="font-bold text-xl">{admin.pseudoName}</h2>
-            <p>{admin.phone}</p>
-            <a href={admin.linkedIn} className="hover:text-white">LinkedIn</a>
+        <div className="flex flex-col justify-center items-center w-[40%] gap-2">
+            <img src={admin.src} alt={admin.name} onClick={() => setIsProfil(!isProfil)} className="rounded-full hover:shadow-[0_0_15px_5px_#C8ACD6]"/>
+            {!isProfil && <div>
+                <h1 className="text-2xl font-extrabold">{admin.name}</h1>
+                <h2 className="font-bold text-xl">{admin.pseudoName}</h2>
+                <p>{admin.phone}</p>
+                <a href={admin.linkedIn} className="hover:text-white">LinkedIn</a>
+            </div>}
         </div>
     </div>
   )
